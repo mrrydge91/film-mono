a37832e4f80149d573515e4318aaa00930db2867
roadboticskevin <kevin@roadbotics.com>
2022-12-05 22:32:50 -0500

    init

--- backend/projects/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-dummy = [{"title": "Rolling Cigs", "writer": "Kevin"}, {"title": "cthulu", "writer": "someone else"}]
-
-def home(request):
-    context = {"films": dummy, "title": "yay"}
-    return render(request, "projects/home.html", context)
